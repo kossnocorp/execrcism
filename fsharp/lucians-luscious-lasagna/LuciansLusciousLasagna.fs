8abf0737125bc5c211eb4e8cf5c3122a7b5a197a
Sasha Koss <koss@nocorp.me>
2024-05-12 07:10:05 +0800

    F# lasagna

---
+++ fsharp/lucians-luscious-lasagna/LuciansLusciousLasagna.fs
@@ -0,0 +1,10 @@
+module LuciansLusciousLasagna
+
+let expectedMinutesInOven = 40
+
+let remainingMinutesInOven ovenTime = expectedMinutesInOven - ovenTime
+
+let preparationTimeInMinutes layers = layers * 2
+
+let elapsedTimeInMinutes layers ovenTime =
+    preparationTimeInMinutes layers + ovenTime
